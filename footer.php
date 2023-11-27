7a145d668a75f471498f37e8cc4d68dc6b9347c2
Eneuem <nemozor@gmail.com>
2023-11-28 00:21:57 +0100

    cest parti

---
+++ footer.php
@@ -0,0 +1,22 @@
+<footer class="flex-column bg-zinc-950 text-white text-lg md:text-2xl p-5 absolute bottom-0 w-full">
+    <div class="flex flex-col items-center justify-evenly md:flex-row">
+        <img src="./img/logo.png" alt="Logo" class="bg-white rounded-lg mb-4 md:mb-0 w-20 h-20">
+        <ul class="flex-column md:flex gap-10 text-center">
+            <li class="mb-2"><a href="">Profil</a></li>
+            <li class="mb-2"><a href="">About</a></li>
+            <li class="mb-2"><a href="">Mention Légales</a></li>
+            <li class="mb-2"><a href="">Contact</a></li>
+        </ul>
+    </div>
+    <div class="flex justify-evenly items-center mt-6">
+        <div>
+            &copy; QuadraStream
+        </div>
+        <div class="flex gap-5 cursor-pointer">
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-github"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-facebook"></i>
+        </div>
+    </div>
+</footer>
