f9a495b4eec7eb0826354b7595cd7b9bbb7cbe02
De Roover Justin <justinderoover@gmail.com>
2023-11-28 19:20:35 +0100

    f

--- PageFilm.php
+++ PageFilm.php
@@ -1,9 +1,34 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-<main class="w-full flex-col">
+<main class="container-lg w-full flex-col">
     <img src="./img/illustration.jpg" class="w-full h-1/2 object-cover">
-    <img src="./img/cover.jpg" class="md:w-52 w-36 rounded-md shadow-md absolute top-72 left-12 md:top-48 md:left-24">
-    <div class="border w-full flex items-center pt-12 pl-12 md:pl-24 md:pt-12">
-        <h1 class="font-bold text-5xl">Joker</h1>
+
+    <div class="border-2 border-black w-full flex pl-12">
+        <img src="./img/cover.jpg" class="w-52 object-contain md:relative md:bottom-36">
+        <div class="flex-col items-center pl-4 w-full">
+            <div class="flex font-bold text-5xl">
+                <h1>Joker</h1>
+                <div class="pl-4">
+                    <span class="material-symbols-outlined cursor-pointer">
+                        favorite
+                    </span>
+                    <span class="material-symbols-outlined">
+                        star
+                    </span>
+                </div>
+            </div>
+            <div class="flex-col mt-4 font-normal text-xl">
+                <div class="flex-col lg:columns-2">
+                    <h5>année :</h5>
+                    <h5>Durée :</h5>
+                    <h5>Genres :</h5>
+                    <h5>Realisateur :</h5>
+                    <h5>Acteurs :</h5>
+                </div>
+                <div class="mt-4 border w-2/3 pr-4 font-normal text-lg">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ab commodi culpa et blanditiis illum porro quibusdam facilis facere debitis expedita, harum dolorum voluptate a, rem ut qui ullam fuga!</p>
+                </div>
+            </div>
+        </div>
     </div>
 </main>
 
