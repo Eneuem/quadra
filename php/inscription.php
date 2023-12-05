7eae5c61ff98ae2363a6cd9b2277355930ee6ee0
Eneuem <nemozor@gmail.com>
2023-12-05 23:50:15 +0100

    inscription fixed

--- php/inscription.php
+++ php/inscription.php
@@ -1,27 +1,30 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require 'db_connect.php';
 // Formulaire d'inscription
 if (isset($_POST['register'])) {
-    $pseudo = htmlspecialchars($_POST['pseudo']);
+    $pseudo = htmlspecialchars($_POST['username']);
     $email = htmlspecialchars($_POST['email']);
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hachage du mot de passe
   
     // Vérifier si le pseudo ou l'email n'est pas déjà utilisé
-    $query = $pdo->prepare("SELECT * FROM users WHERE pseudo = ? OR email = ?");
+    $query = $pdo->prepare("SELECT * FROM users WHERE username = ? OR email = ?");
     $query->execute([$pseudo, $email]);
   
     if ($query->rowCount() == 0) {
         // Insertion des données dans la base de données
-        $insert = $pdo->prepare("INSERT INTO users (pseudo, email, password) VALUES (?, ?, ?)");
-        $insert->execute([$pseudo, $email, $password]);
+        $insert = $pdo->prepare("INSERT INTO users (username, email, password, user_power) VALUES (?, ?, ?, ?)");
+        $insert->execute([$pseudo, $email, $password, 0]);
   
         if ($insert) {
             echo "Inscription réussie.";
   
             // Stockez des informations sur l'utilisateur dans la session
             $_SESSION['user_id'] = $pdo->lastInsertId();
-            $_SESSION['pseudo'] = $pseudo;
+            $_SESSION['username'] = $pseudo;
             $_SESSION['email'] = $email;
         } else {
             echo "Erreur lors de l'inscription.";
