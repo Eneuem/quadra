b6f15a163baf9331469c37bf0c35c7740193615f
Eneuem <nemozor@gmail.com>
2023-12-11 09:55:45 +0100

    letsgo

--- header.php
+++ header.php
@@ -15,7 +15,13 @@ $currentUrl = $_SERVER['REQUEST_URI'];
 <link rel="stylesheet" href="css/style.css">
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script> <!-- Inclure jQuery -->
 
-<style></style>
+<style>
+.active-link {
+    color: #f74366;
+    border-bottom: 2px solid #f74366;
+}
+
+</style>
 
 <!-- Nav Container -->
 
@@ -32,7 +38,7 @@ $currentUrl = $_SERVER['REQUEST_URI'];
             <div class="flex items-center space-x-6">
 
                 <!-- Logo -->
-                <img src="./img/logo.png" class="w-10 h-10 bg-white rounded-full" alt="Quadra Logo" />
+                <a href="index.php"><img src="./img/logo.png" class="w-10 h-10 bg-white rounded-full" alt="Quadra Logo" /></a>
 
                 <!-- Search -->
                 <div>
@@ -72,9 +78,9 @@ $currentUrl = $_SERVER['REQUEST_URI'];
                         <div id="loginBtn" class="hover:text-lime-100">Login</div>
                     </a>
                     <a href="" id="signupBtn" data-page="signup" class="dynamic-load px-4 py-1 font-bold text-lime-100 bg-yellow-400 rounded-xl hover:opacity-70">Sign Up</a>
-                    <div id="userName" class="hidden hover:text-lime-100"><?php echo $_SESSION['username']; ?></div>
-                    <div id="userIcon">
-                        <svg class="hidden text-yellow-400 h-8 w-8 top-3 fill-current" xmlns="http://www.w3.org/2000/svg" height="34" viewBox="0 -960 960 960" width="24">
+                    <div id="userName" class="hidden hover:text-lime-100 cursor-pointer"><?php echo $_SESSION['username']; ?></div>
+                    <div id="userIcon" class="hidden">
+                        <svg class="text-yellow-400 h-8 w-8 top-3 fill-current" xmlns="http://www.w3.org/2000/svg" height="34" viewBox="0 -960 960 960" width="24">
                             <path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z"></path>
                         </svg>
                     </div>
@@ -121,16 +127,6 @@ $currentUrl = $_SERVER['REQUEST_URI'];
         </div>
     </nav>
 
-    <section class="cta mt-5">
-        <div class="mx-auto max-w-screen-xl sm:py-16 lg:px-1">
-            <div class=" text-center">
-                <h2 style="color: #f74366; font-size: 82px;" class="mb-4 text-4xl tracking-tight font-extrabold leading-tight">Meett your TV concierge.</h2>
-                <p class="mb-12 text-white md:text-2xl mx-auto max-w-screen-md">Need something to watch? We’re on it. Plex combines free movies & TV with your favorite streaming services, so there’s always more to discover.</p>
-                <a href="#" class="text-gray-900 bg-yellow-400 hover:opacity-70 md:text-2xl font-bold rounded-full text-sm px-5 py-2.5 mr-2 mb-2">Start Streaming</a>
-            </div>
-        </div>
-    </section>
-</div>
 
 <?php include("js/header_script.php"); ?>
 
--- php/comp/cta.php
+++ php/comp/cta.php
@@ -1,10 +1,12 @@
-<section class="bg-yellow-400 rounded-xl">
-        <div class="py-4 px-2 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-            <div class="mx-auto max-w-screen-sm text-center">
-                <h2 class="mb-4 text-4xl tracking-tight font-extrabold leading-tight text-gray-900 ">Start your free trial today</h2>
-                <p class="mb-6 text-white md:text-lg">Try quadra streaming for 30 days free. No credit card required.</p>
 
-                <a href="#" class="text-yellow-400 bg-blue-800 hover:opacity-70 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ">Free trial for 30 days</a>
+    <section class="cta mt-5">
+        <div class="mx-auto max-w-screen-xl sm:py-16 lg:px-1">
+            <div class=" text-center">
+                <h2 style="color: #f74366; font-size: 82px;" class="mb-4 text-4xl tracking-tight font-extrabold leading-tight">Meet your movie buddy</h2>
+                <br>
+                <p class="mb-12 text-white md:text-2xl mx-auto max-w-screen-md">Need something to watch? We’re on it. Quadra combines free movies & TV with your favorite streaming services, so there’s always more to discover.</p>
+                <a href="#" class="text-gray-900 bg-yellow-400 hover:opacity-70 md:text-2xl font-bold rounded-full text-sm px-5 py-2.5 mr-2 mb-2">Start Streaming</a>
             </div>
         </div>
-</section> 
+    </section>
+</div>
