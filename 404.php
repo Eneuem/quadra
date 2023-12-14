c413422f5d2880f38f917a32c5f5a6767f5e59a4
Eneuem <nemozor@gmail.com>
2023-12-14 13:36:21 +0100

    snap before Stripes

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php
+http_response_code(404);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page non trouvée</title>
+    <!-- Incluez ici le CSS nécessaire -->
+</head>
+<body>
+    <h1>Oups! Page non trouvée</h1>
+    <p>Désolé, la page que vous cherchez n'existe pas ou a été déplacée.</p>
+    <a href="/">Retourner à la page d'accueil</a>
+</body>
+</html>
